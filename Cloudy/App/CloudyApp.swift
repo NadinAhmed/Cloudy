0779c9a5c69ff1c84551b4ebd8fc2d53adcfc8f1
NadinAhmed <nadinahmed316@gmail.com>
2026-06-14 07:44:29 -0700

    add font to all app

--- Cloudy/App/CloudyApp.swift
+++ Cloudy/App/CloudyApp.swift
@@ -5,11 +5,21 @@
 //  Created by Nadin Ahmed on 10/06/2026.
 //
 
-import SwiftUI
 import SwiftData
+import SwiftUI
 
 @main
 struct CloudyApp: App {
+    init() {
+        let appearance = UINavigationBar.appearance()
+        appearance.largeTitleTextAttributes = [
+            .font: SwiftUI.Font.custom("Inter-Bold", size: 34)
+        ]
+        appearance.titleTextAttributes = [
+            .font: SwiftUI.Font.custom("Inter-SemiBold", size: 17)
+        ]
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
--- Cloudy/Core/Theme/AppFont.swift
+++ Cloudy/Core/Theme/AppFont.swift
@@ -8,15 +8,13 @@
 import Foundation
 import SwiftUI
 
-struct AppFont{
-    struct Font {
-            static let cityName = SwiftUI.Font.custom("Inter-Bold", size: 34)
-            static let temperature = SwiftUI.Font.custom("Inter-Medium", size: 96)
-            static let condition = SwiftUI.Font.custom("Inter-Regular", size: 20)
-            static let highLow = SwiftUI.Font.custom("Inter-Regular", size: 15)
-            static let forecastTitle = SwiftUI.Font.custom("Inter-SemiBold", size: 12)
-            static let forecastRow = SwiftUI.Font.custom("Inter-Regular", size: 17)
-            static let cardLabel = SwiftUI.Font.custom("Inter-SemiBold", size: 12)
-            static let cardValue = SwiftUI.Font.custom("Inter-Bold", size: 22)
-        }
+struct AppFont {
+    static let cityName = SwiftUI.Font.custom("Inter-Bold", size: 34)
+    static let temperature = SwiftUI.Font.custom("Inter-Medium", size: 96)
+    static let condition = SwiftUI.Font.custom("Inter-Regular", size: 20)
+    static let highLow = SwiftUI.Font.custom("Inter-Regular", size: 15)
+    static let forecastTitle = SwiftUI.Font.custom("Inter-SemiBold", size: 12)
+    static let forecastRow = SwiftUI.Font.custom("Inter-Regular", size: 17)
+    static let cardLabel = SwiftUI.Font.custom("Inter-SemiBold", size: 12)
+    static let cardValue = SwiftUI.Font.custom("Inter-Bold", size: 22)
 }
